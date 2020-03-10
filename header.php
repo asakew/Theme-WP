e73086f21c1653d826d7bfaf64afbc6518238f79
ASA <asauz777@gmail.com>
2020-03-10 14:49:01 +0500

    meta: viewport
    sahifa ekranlarga moslashuvchanligini oshirish uchun kerak...

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body>
 	
