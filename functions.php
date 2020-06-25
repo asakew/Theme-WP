0c4e6c5c0f409ca05831e2d5b4ac1e028c6652ee
Akmal Sindorov <46191008+asakew@users.noreply.github.com>
2020-06-25 18:41:53 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,12 +3,11 @@
 function asabest_scripts() {
 	// up ? on bu wp_head();
 	wp_enqueue_style( 'asabest', get_stylesheet_uri() );
-	
 	wp_enqueue_style('bootstrap.min',  get_template_directory_uri() . '/assets/css/bootstrap.min.css');
 
 	// down / on by wp_footer();
-  wp_enqueue_script('popper.min', get_template_directory_uri() . '/assets/js/popper.min.js', [], null, true);
-  wp_enqueue_style('bootstrap.min',  get_template_directory_uri() . '/assets/js/bootstrap.min.js', [], null, true);
+  	wp_enqueue_script('popper.min', get_template_directory_uri() . '/assets/js/popper.min.js', [], null, true);
+  	wp_enqueue_style('bootstrap.min',  get_template_directory_uri() . '/assets/js/bootstrap.min.js', [], null, true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
